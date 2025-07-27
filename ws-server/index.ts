952a5db62503fe948cab445bf1907706eb6cba79
Vignesh D <vignesh.d9123@gmail.com>
2025-07-27 22:11:18 +0530

    Send messages to socket using userid

--- ws-server/index.ts
+++ ws-server/index.ts
@@ -1,9 +1,10 @@
 import  {WebSocketServer, WebSocket} from 'ws'
 
 const wss = new WebSocketServer({ port: parseInt(process.env.PORT!) || 8080 })
-const rooms = new Map<Number, WebSocket[]>()
+const rooms = new Map<Number, string[]>()
 const wsRooms = new Map<WebSocket, Number>()
 const usersWs = new Map<String, WebSocket>()
+const wsUsers = new Map<WebSocket, string>()
 const relayer_ws = new WebSocket('ws://localhost:3000')
 
 relayer_ws.onmessage = (message) => {
@@ -11,25 +12,27 @@ relayer_ws.onmessage = (message) => {
     const messageObj = JSON.parse(message.data.toString())
     rooms.get(messageObj.from!)?.forEach((client) => {
         console.log("Sending message to client", client)
-        client.send(JSON.stringify(messageObj), {
+        usersWs.get(client)?.send(JSON.stringify(messageObj), {
             binary: false
         })
     })
 }
 wss.on('connection', (ws, req) => {
     console.log("Socket connected to server", process.env.PORT)
-    console.log("Socket connected to client", req.headers['user-id'])
     if(!req.headers['user-id'] ){
         ws.close()
     }
+    const userId = req.headers['user-id'] as string
+    usersWs.set(userId, ws)
+    wsUsers.set(ws, userId)
     ws.on('message', (message: string) => {
         const messageObj = JSON.parse(message)
         if(messageObj.type == "joinRoom"){
             if(rooms.has(messageObj.roomId) && rooms.get(messageObj.roomId) != null){
-                rooms.get(messageObj.roomId)?.push(ws)
+                rooms.get(messageObj.roomId)?.push(wsUsers.get(ws)!)
                 wsRooms.set(ws, messageObj.roomId)  
             }else{
-                rooms.set(messageObj.roomId, [ws])
+                rooms.set(messageObj.roomId, [wsUsers.get(ws)!])
                 wsRooms.set(ws, messageObj.roomId)  
             }
             console.log("Joined room", messageObj.roomId , "with", rooms.get(messageObj.roomId)?.length, "clients")
@@ -37,10 +40,10 @@ wss.on('connection', (ws, req) => {
         if(messageObj.type == "message"){
             if(wsRooms.has(ws)){
                 rooms.get(wsRooms.get(ws)!)?.forEach((client) => {
-                    if(client == ws){
+                    if(usersWs.get(client) == ws){
                         return
                     }
-                    client.send(message, {
+                    usersWs.get(client)?.send(message, {
                         binary: false
                     })
                 })
